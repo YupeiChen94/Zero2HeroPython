c1b5f49db3062b04ed2903bf9e6b0c38296534cf
YupeiChen <ychen@my.centenary.edu>
2021-11-21 13:40:56 -0600

    .

--- Advanced/Modules/regex_module.py
+++ Advanced/Modules/regex_module.py
@@ -40,6 +40,27 @@ print(match)
 
 # Exclusion
 phrase = 'There are 3 numbers 34 inside 5 this sentence'
-pattern = r'[^\d]'
+pattern = r'[^\d]+'
 match = re.findall(pattern, phrase)
 print(match)
+
+phrase = 'This is a string! But it has punctuation. How can we remove it?'
+pattern = r'[^!.? ]+'
+match = re.findall(pattern, phrase)
+print(match)
+print(' '.join(match))
+
+# Inclusion
+phrase = 'Only find the hypen-words in this sentence. But you do not know how long-ish they are.'
+pattern = r'[\w]+-[\w]+'
+match = re.findall(pattern, phrase)
+print(match)
+
+# Multi pattern grouping
+phrase = 'Hello, would you like some catfish?'
+phrase_two = 'Hello, woul dyou like to take a catnap?'
+phrase_three = 'Hello, have you seen this caterpillar?'
+pattern = r'cat(fish|nap|erpillar)'
+print(re.search(pattern, phrase))
+print(re.search(pattern, phrase_two))
+print(re.search(pattern, phrase_three))
