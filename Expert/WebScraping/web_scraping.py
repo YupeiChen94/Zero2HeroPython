5e4ba83ca74c7db2d40547795a29fe0c87817424
YupeiChen <ychen@my.centenary.edu>
2021-11-22 14:24:56 -0600

    .

--- Expert/WebScraping/web_scraping.py
+++ Expert/WebScraping/web_scraping.py
@@ -10,4 +10,11 @@ soup = bs4.BeautifulSoup(result.text, 'lxml')
 site_title = soup.select('title')[0].getText()
 site_paragrphs = soup.select('p')
 
+# Grabbing text from table of contents
+result = requests.get('https://en.wikipedia.org/wiki/Grace_Hopper')
+soup = bs4.BeautifulSoup(result.text, 'lxml')
+toc_text = soup.select('.toctext')
+# print(toc_text)
+for item in toc_text:
+    print(item.text)
 
