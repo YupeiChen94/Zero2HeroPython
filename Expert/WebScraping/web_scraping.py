b4034e7a95e969f7cfd4f9a99280c9b96d7ce4de
YupeiChen <ychen@my.centenary.edu>
2021-11-22 14:17:35 -0600

    .

--- Expert/WebScraping/web_scraping.py
+++ Expert/WebScraping/web_scraping.py
@@ -1,3 +1,13 @@
 import requests
 import bs4
 
+result = requests.get('https://www.example.com/')
+print(type(result))
+# print(result.text)
+
+soup = bs4.BeautifulSoup(result.text, 'lxml')
+# print(soup)
+site_title = soup.select('title')[0].getText()
+site_paragrphs = soup.select('p')
+
+
