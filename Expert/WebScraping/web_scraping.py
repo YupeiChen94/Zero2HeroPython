1a3d980bf83df157e7630214343658f485a61d94
YupeiChen <ychen@my.centenary.edu>
2021-11-23 14:13:11 -0600

    .

--- Expert/WebScraping/web_scraping.py
+++ Expert/WebScraping/web_scraping.py
@@ -16,5 +16,19 @@ soup = bs4.BeautifulSoup(result.text, 'lxml')
 toc_text = soup.select('.toctext')
 # print(toc_text)
 for item in toc_text:
-    print(item.text)
+    # print(item.text)
+    pass
+
+res = requests.get('https://en.wikipedia.org/wiki/Deep_Blue_(chess_computer)')
+soup = bs4.BeautifulSoup(res.text, 'lxml')
+
+computer = soup.select('.thumbimage')[0]['src']
+print(computer)
+image_link = requests.get(f"https:{computer}")
+# print(image_link.content)
+with open('my_computer_image.jpg', mode='wb') as f:
+    f.write(image_link.content)
+
+
+
 
