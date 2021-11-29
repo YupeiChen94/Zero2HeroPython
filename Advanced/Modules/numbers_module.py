79934cc4ee2af4e37db88263b3dca0606d86943b
YupeiChen <ychen@my.centenary.edu>
2021-11-29 16:53:52 -0600

    .

---
+++ Advanced/Modules/numbers_module.py
@@ -0,0 +1,15 @@
+# Hex representation  of number
+print(12, hex(12))
+
+# Binary representation of number
+print(12, bin(12))
+
+# Power function
+print(pow(2,4))
+print(pow(2,4,3))
+
+# Absolute value
+print(-2, abs(-2))
+
+# Rounding
+print(3.141592, round(3.14, 2))
